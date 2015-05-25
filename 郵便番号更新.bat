d2da5fb611e9a9941a8e98fd5cad3597488c7c43
Cuhey3 <uotaneet@gmail.com>
2015-05-25 14:15:43 +0900

    Update 郵便番号更新.bat

--- 郵便番号更新.bat
+++ 郵便番号更新.bat
@@ -1,1 +1,9 @@
+# このbatファイルについては以下をお読みください。
+# https://github.com/Cuhey3/getjushojp/blob/master/README.md
+# 
+# このbatファイルを実行するには、
+# batファイルと同階層にprogramフォルダがあること、
+# programフォルダの中にGetJushoJP-1.0-SNAPSHOT-jar-with-dependencies.jarがあること、
+# さらにJava1.7以降がインストールされている必要があります。
+
 java -jar program/GetJushoJP-1.0-SNAPSHOT-jar-with-dependencies.jar
